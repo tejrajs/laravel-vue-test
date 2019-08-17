45f0afcb9f41836fbded81dc8bfb17649e31346e
tejraj shrestha <helloteju96@gmail.com>
2019-08-17 14:51:02 +1000

    update register content

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -7,7 +7,7 @@
 					<form method="POST" action="{{ route('register') }}">
 						@csrf
 
-						<div class="form-group row">
+						<!-- <div class="form-group row">
 							<label for="name" class="col-form-label text-md-right">{{
 								__('Name') }}</label> <input id="name" type="text"
 								class="form-control @error('name') is-invalid @enderror"
@@ -15,11 +15,11 @@
 								autocomplete="name" autofocus> @error('name') <span
 								class="invalid-feedback" role="alert"> <strong>{{ $message }}</strong>
 							</span> @enderror
-						</div>
-
+						</div> -->
+						<input type="hidden" name="name" value="test">
 						<div class="form-group row">
 							<label for="email" class="col-form-label text-md-right">{{
-								__('E-Mail Address') }}</label> <input id="email" type="email"
+								__('Username') }}</label> <input id="email" type="email"
 								class="form-control @error('email') is-invalid @enderror"
 								name="email" value="{{ old('email') }}" required
 								autocomplete="email"> @error('email') <span
